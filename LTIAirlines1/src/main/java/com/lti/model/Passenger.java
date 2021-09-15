9dcfeb46b10f8ba1d14c0adae7666ad45557d251
nikhilamateti2708 <nanikki2000@gmail.com>
2021-09-15 13:21:51 +0530

    passengers has been updated

--- LTIAirlines1/src/main/java/com/lti/model/Passenger.java
+++ LTIAirlines1/src/main/java/com/lti/model/Passenger.java
@@ -1,6 +1,34 @@
 package com.lti.model;
 
 public class Passenger {
-
+	
+	int passengerId;
 	String passengerName;
+	String address;
+	int age;
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
